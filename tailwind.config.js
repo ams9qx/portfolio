bfcdd6bb93a78ccfe375d1ad38e0ea541d476972
ams9qx <62727459+ams9qx@users.noreply.github.com>
2021-07-08 16:33:22 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors:{
+      transparent: 'transparent',
+      current: 'currentColor',
+      custom:{
+
+      grey:{
+        darkest: '#47464e',
+        darker: '#63626a',
+        dark: '#717076',
+        DEFAULT: '#b7b6bc',
+        light: '#ededef'
+      }
+    }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  
+}
